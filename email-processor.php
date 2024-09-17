fc742f1016696f8579bbf6889f53e78e0c6769f0
Tamim  Rashid <rashidytamimu@gmail.com>
2024-09-17 08:48:22 +0300

    update

--- email-processor.php
+++ email-processor.php
@@ -12,12 +12,13 @@ function email_verification($fullname, $email, $email_subject,  $email_template)
         $mail->SMTPDebug = 2; // Enable detailed debug output
 
         $mail->isSMTP();                                            
-        $mail->Host       = 'smtp.gmail.com';                        
-        $mail->SMTPAuth   = true;                                
-        $mail->Username   = 'taliantamten@gmail.com';  // sender email               
-        $mail->Password   = 'nanz sgco kefd ogfz';  // sender password or key                          
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;           
-        $mail->Port       = 465;                                    
+        $mail->Host       = 'smtp.gmail.com'; //Set the SMTP server to send                       
+        $mail->SMTPAuth   = true; //Enable SMTP authentication                               
+        $mail->Username   = 'taliantamten@gmail.com';  // sender email or SMTP username             
+        $mail->Password   = 'nanz sgco kefd ogfz';  // sender password or key  or SMTP password                        
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption          
+        $mail->Port       = 465;      //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                              
 
         $mail->setFrom('taliantamten@gmail.com', $fullname);
         $mail->addAddress($email); 
