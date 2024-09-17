e88dc96e9f84e4768f116d5e09a228abf4225c4d
Tamim  Rashid <rashidytamimu@gmail.com>
2024-09-17 09:04:40 +0300

    update

--- contact.php
+++ contact.php
@@ -11,10 +11,30 @@
     <title>Contact-form</title>
   </head>
   <body>
-    <section id="footer">
-        <div class="container-fluid">
-            <div class="row bg-light">
+    <section id="main">
 
+    </section>
+    <section id="footer">
+        <div class="container-fluid mt-5 my-5 justify-content-end">
+            <div class="row bg-light">
+                <div class="col-lg-4 py-5">
+                    <form action="#" method="post">
+                            <label  class="text-center text-muted mb-2"for="contactform">Contact Form</label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">@</span>
+                            <input type="text" class="form-control" placeholder="Your name" aria-label="Username" name="username" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">@</span>
+                            <input type="email" class="form-control" placeholder="email" aria-label="Username" name="email" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group">
+                            <span class="input-group-text">With textarea</span>
+                            <textarea class="form-control" name="message" aria-label="Your message"></textarea>
+                        </div>
+                        <button class="btn btn-secondary my-2" type="submit">Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
     </section>
